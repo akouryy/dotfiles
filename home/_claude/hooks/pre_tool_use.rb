8714c3b4f1fc80808e8a1ac11625c7e2042db954
akouryy <akouryy7@gmail.com>
2026-03-11 01:57:39 +0900

    Prohibit Claude Code from running git stash

--- home/_claude/hooks/pre_tool_use.rb
+++ home/_claude/hooks/pre_tool_use.rb
@@ -8,3 +8,8 @@ if command.include?('2>/dev/null')
   $stderr.puts 'No ignoring stderr'
   exit 2
 end
+
+if command.match?(/\bgit\s+stash\b/)
+  $stderr.puts 'git stash is prohibited'
+  exit 2
+end
